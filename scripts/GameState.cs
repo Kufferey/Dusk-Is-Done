e1517ff616ca4be0258a9f4f0cae946f11cb9d92
Kufferey <109652222+Kufferey@users.noreply.github.com>
2024-09-09 22:45:32 -0400

    Add files via upload

--- scripts/GameState.cs
+++ scripts/GameState.cs
@@ -6,26 +6,20 @@ public partial class GameState : Node3D
 	[Signal]
 	public delegate void NewDayEventHandler();
 
-	[Export]
-	public int playerScore = 0;
+	public static int currentDay;
+
+	public static int playerScore = 0;
+
 	[Export]
 	public InteractableObject playerCurrentHoveredObject;
 	[Export]
 	public InteractableObject playerCurrentHeldItem;
-	
-	[Export]
-	public int currentDay;
 
-	private InteractableObjectManager interactableObjectManager;
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		interactableObjectManager = GetNode<InteractableObjectManager>("/root/InteractableObjectManager");
-		GD.Print(interactableObjectManager.interactableObjectPrefabs);
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		
@@ -33,7 +27,7 @@ public partial class GameState : Node3D
 
 	public void AddTableItem(InteractableObject.InteractableObjectType type)
 	{
-		Node tableItem = interactableObjectManager.interactableObjectPrefabs[type].Instantiate();
+		Node tableItem = InteractableObjectManager.interactableObjectPrefabs[type].Instantiate();
 		GetNode<Node>("Interactables").AddChild(tableItem);
 	}
 }
--- scripts/interactables/InteractableObjectManager.cs
+++ scripts/interactables/InteractableObjectManager.cs
@@ -5,16 +5,16 @@ using System;
 public partial class InteractableObjectManager : Node
 {
 	// List of all items in the game.
-    public Godot.Collections.Dictionary<InteractableObject.InteractableObjectType, PackedScene> interactableObjectPrefabs {get; set;} = new Godot.Collections.Dictionary<InteractableObject.InteractableObjectType, PackedScene>
+    public static Godot.Collections.Dictionary<InteractableObject.InteractableObjectType, PackedScene> interactableObjectPrefabs {get; set;} = new Godot.Collections.Dictionary<InteractableObject.InteractableObjectType, PackedScene>
 	{
-		// {InteractableObject.InteractableObjectType.Cherry,        (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
-		// {InteractableObject.InteractableObjectType.CherrySpoiled, (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
-		// {InteractableObject.InteractableObjectType.MedicalPills,  (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
-		// {InteractableObject.InteractableObjectType.Pills,         (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
+		{InteractableObject.InteractableObjectType.Cherry,        (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
+		{InteractableObject.InteractableObjectType.CherrySpoiled, (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
+		{InteractableObject.InteractableObjectType.MedicalPills,  (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
+		{InteractableObject.InteractableObjectType.Pills,         (Godot.PackedScene)ResourceLoader.Load<PackedScene>("")},
 	};
 
 	// Add a interaction item to "interactableObjectPrefabs".
-    public void AddInteractableItemsToPrefabs(InteractableObject.InteractableObjectType type, String scenePath)
+    public static void AddInteractableItemsToPrefabs(InteractableObject.InteractableObjectType type, String scenePath)
 	{
 		interactableObjectPrefabs.Add((InteractableObject.InteractableObjectType)type, (Godot.PackedScene)ResourceLoader.Load<PackedScene>(scenePath));
 	}
